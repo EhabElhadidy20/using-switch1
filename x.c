666ddcb854bb623701595460cd42ae42defa0ccb
Ehab El hadidy <140009634+EhabElhadidy20@users.noreply.github.com>
2023-07-20 00:36:52 +0300

    Add files via upload

---
+++ x.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void main ()
+{
+		// quiz 
+	
+ int num1 ;
+ printf ("Enter number  ");
+ scanf ("%d",&num1);
+ switch (num1)
+ {
+	 case 1 :
+	 printf ("number = 1");
+	 break;
+	 case 2 :
+	 printf ("number = 2");
+	 break ;
+	 case 3 :
+	 printf ("number = 3");
+	 break ;
+	  case 4 :
+	 printf ("number = 4");
+	 break;
+	  case 5 :
+	 printf ("number = 5");
+	 break;
+	 default :
+	 printf ("incorrect ");
+	 
+	 
+ }
+}
